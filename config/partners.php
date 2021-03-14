042eee3ecb2e13cf520d6eb0ff4c48ff4a54967d
Vaalyn <vaalyndev@gmail.com>
2021-03-14 17:31:13 +0100

    Update partners

--- config/partners.php
+++ config/partners.php
@@ -5,10 +5,6 @@ return [
 		'banner' => 'galacon_banner.png',
 		'url' => 'https://www.galacon.eu'
 	],
-	'ponymerch' => [
-		'banner' => 'ponymerch_banner.png',
-		'url' => 'https://ponymerch.com/'
-	],
 	'bronies_de' => [
 		'banner' => 'bronies_de_banner.png',
 		'url' => 'https://www.bronies.de/'

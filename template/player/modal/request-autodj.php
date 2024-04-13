028ead3573364a1b62593fafa9a7ae35a7e36106
Vaalyn <vaalyndev@gmail.com>
2024-04-13 13:51:32 +0200

    Remove some links

--- template/player/modal/request-autodj.php
+++ template/player/modal/request-autodj.php
@@ -15,16 +15,6 @@
 		<?php endif; ?>
 
 		<div class="row">
-			<div class="col s12 m6 center">
-				<a href="https://brg.drweissbrot.net/autodj/add" class="btn-flat blue-text text-lighten-1">Song für AutoDj vorschlagen</a>
-			</div>
-
-			<div class="col s12 m6 center">
-				<a href="https://brg.drweissbrot.net/autodj/remove" class="btn-flat blue-text text-lighten-1">Song aus AutoDj entfernen</a>
-			</div>
-		</div>
-
-		<div class="row">
 			<div class="col s12">
 				<div class="row">
 					<div class="input-field col s12 m4">
--- template/player/modal/request-offline.php
+++ template/player/modal/request-offline.php
@@ -1,16 +1,6 @@
 <div id="brg-player-system-modal" class="modal modal-fixed-footer grey darken-4">
 	<div class="modal-content">
 		<div class="row">
-			<div class="col s12 m6 center">
-				<a href="https://brg.drweissbrot.net/autodj/add" class="btn-flat blue-text text-lighten-1">Song für AutoDj vorschlagen</a>
-			</div>
-
-			<div class="col s12 m6 center">
-				<a href="https://brg.drweissbrot.net/autodj/remove" class="btn-flat blue-text text-lighten-1">Song aus AutoDj entfernen</a>
-			</div>
-		</div>
-
-		<div class="row">
 			<div class="col s12 center">
 				<img src="<?php echo $baseUrl; ?>image/request-system-offline.gif" alt="Das Request System ist deaktiviert" />
 			</div>
